8de9223452b60617e8b2c6d09f592d7a9ab7936f
MuhBayu <bnugraha00@hotmail.com>
2018-10-18 17:11:34 +0700

    styling

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,8 +7,6 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import ImageSearch from '@material-ui/icons/ImageSearch';
-// import logo from '../logo.svg';
-import '../App.css';
 
 const styles = {
   root: {
